3bf96ca25dfd55629646cc270f973ba067df1e16
ChuxMg <chuxmgbojikwe@gmail.com>
2024-07-01 06:00:11 +0700

    Adding a Header Button

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
-
+import IconButton from "./components/UI/IconButton";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,11 +13,23 @@ export default function App() {
       <StatusBar style="auto" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="AllPlaces" component={AllPlaces} />
-          <Stack.Screen name="AddPlaces" component={AddPlace} />
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add"
+                  size={24}
+                  color={tintColor}
+                  onPress={() => navigation.navigate("AddPlace")}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen name="AddPlace" component={AddPlace} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
   );
 }
-
