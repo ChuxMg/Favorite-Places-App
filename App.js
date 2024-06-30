efaddeb3d7dcd6ccca68abd09aaabdc325697d89
ChuxMg <chuxmgbojikwe@gmail.com>
2024-07-01 06:35:30 +0700

    Global Colors & Styling

--- App.js
+++ App.js
@@ -4,19 +4,25 @@ import { StatusBar } from "expo-status-bar";
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
 import IconButton from "./components/UI/IconButton";
+import { Colors } from "./constants/colors";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+          headerStyle: {backgroundColor: Colors.primary500},
+          headerTintColor: Colors.gray700,
+          contentStyle: {backgroundColor: Colors.gray700}
+        }}>
           <Stack.Screen
             name="AllPlaces"
             component={AllPlaces}
             options={({ navigation }) => ({
+              title: "Your Favorite Places",
               headerRight: ({ tintColor }) => (
                 <IconButton
                   icon="add"
@@ -27,7 +33,13 @@ export default function App() {
               ),
             })}
           />
-          <Stack.Screen name="AddPlace" component={AddPlace} />
+          <Stack.Screen
+            name="AddPlace"
+            component={AddPlace}
+            options={{
+              title: "Add a new Place",
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
---
+++ constants/colors.js
@@ -0,0 +1,11 @@
+export const Colors = {
+  primary50: "#F3E5F5",
+  primary100: "#E1BEE7",
+  primary200: "#CE93D8",
+  primary400: "#AB47BC",
+  primary500: "#9C27B0",
+  primary700: "#7B1FA2",
+  primary800: "#6A1B9A",
+  accent500: "#8BC34A",
+  gray700: "#212121",
+};
