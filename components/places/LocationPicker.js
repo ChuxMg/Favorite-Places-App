2c7d3cb78c21f9682069fc71afaffab14adb3f84
ChuxMg <chuxmgbojikwe@gmail.com>
2024-07-02 09:31:11 +0700

    Adding a Location Preview Map

--- components/places/LocationPicker.js
+++ components/places/LocationPicker.js
@@ -1,59 +1,85 @@
-import { Alert, StyleSheet, View } from "react-native";
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
 import OutlinedButton from "../UI/OutlinedButton";
 import { Colors } from "../../constants/colors";
-import { getCurrentPositionAsync, PermissionStatus, useForegroundPermissions } from "expo-location";
+import {
+  getCurrentPositionAsync,
+  PermissionStatus,
+  useForegroundPermissions,
+} from "expo-location";
+import { useState } from "react";
+import { getMapPreview } from "../../util/location";
 
 function LocationPicker() {
-    const [locationPermissionInformation, requestPermission] = useForegroundPermissions();
+  const [pickedLocation, setPickedLocation] = useState();
 
-    async function verifyPermissions() {
-        if (
-          locationPermissionInformation.status === PermissionStatus.UNDETERMINED
-        ) {
-          const permissionResponse = await requestPermission();
+  const [locationPermissionInformation, requestPermission] =
+    useForegroundPermissions();
 
-          return permissionResponse.granted;
+  async function verifyPermissions() {
+    if (
+      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissionResponse = await requestPermission();
 
-        }
-
-        if (locationPermissionInformation.status === PermissionStatus.DENIED) {
-          Alert.alert(
-            "Insufficient Permissions!",
-            "You need to grant location permission to use this app."
-          );
-
-          return false;
-        }
-
-        return true;
+      return permissionResponse.granted;
     }
 
-    async function getLocationHandler() {
-        const hasPermission = await verifyPermissions();
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant location permission to use this app."
+      );
 
-        if (!hasPermission) {
-            return;
-        }
-
-        const location = await getCurrentPositionAsync();
-        console.log(location);
+      return false;
     }
 
-    function pickOnMapHandler() {}
+    return true;
+  }
 
-    return (
-      <View>
-        <View style={styles.mapPreview}></View>
-        <View style={styles.actions}>
-          <OutlinedButton icon="location" onPress={getLocationHandler}>
-            Locate User
-          </OutlinedButton>
-          <OutlinedButton icon="map" onPress={pickOnMapHandler}>
-            Pick on Map
-          </OutlinedButton>
-        </View>
-      </View>
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    const location = await getCurrentPositionAsync();
+    setPickedLocation({
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+    });
+
+    console.log(location);
+  }
+
+  function pickOnMapHandler() {}
+
+  let locationPreview = <Text>No location picked yet.</Text>;
+
+  if (pickedLocation) {
+    locationPreview = (
+      <Image
+        style={styles.image}
+        source={{
+          uri: getMapPreview(pickedLocation.lat, pickedLocation.lng),
+        }}
+      />
     );
+  }
+
+  return (
+    <View>
+      <View style={styles.mapPreview}>{locationPreview}</View>
+      <View style={styles.actions}>
+        <OutlinedButton icon="location" onPress={getLocationHandler}>
+          Locate User
+        </OutlinedButton>
+        <OutlinedButton icon="map" onPress={pickOnMapHandler}>
+          Pick on Map
+        </OutlinedButton>
+      </View>
+    </View>
+  );
 }
 
 export default LocationPicker;
@@ -67,10 +93,16 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: Colors.primary100,
     borderRadius: 4,
+    overflow: "hidden"
   },
   actions: {
     flexDirection: "row",
     justifyContent: "space-around",
-    alignItems: "center"
+    alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    // borderRadius: 4
   },
 });
