20a30fcf88bea82fd4cf6b65203be8830f7ca0c2
ChuxMg <chuxmgbojikwe@gmail.com>
2024-07-03 12:19:03 +0700

    Passing Entered Data to the "AllPlaces" Screen and Outputting a List Of Places

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
 import PlacesList from "../components/places/PlacesList";
+import { useIsFocused } from "@react-navigation/native";
 
-function AllPlaces() {
-  return <PlacesList />;
+function AllPlaces({route}) {
+  const [loadedPlaces, setLoadedplaces] = useState([]);
+
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (isFocused && route.params) {
+      setLoadedplaces(curPlaces => [...curPlaces, route.params.place]);
+    }
+  }, [isFocused, route]);
+
+  return <PlacesList places={loadedPlaces} />;
 }
 
 export default AllPlaces;
