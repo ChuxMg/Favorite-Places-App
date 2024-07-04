0a5979d70b4d57042671973af2bb44ab22dfa0ce
ChuxMg <chuxmgbojikwe@gmail.com>
2024-07-05 05:21:10 +0700

    Styling Place Items

--- components/places/PlacesList.js
+++ components/places/PlacesList.js
@@ -3,18 +3,19 @@ import PlacesItem from "./PlacesItem";
 import { Colors } from "../../constants/colors";
 
 function PlacesList({ places }) {
-    if (!places || places.length === 0) {
-        return (
-          <View style={styles.fallbackContainer}>
-            <Text style={styles.fallbackText}>
-              No places added yet - start adding some!
-            </Text>
-          </View>
-        );
-    }
+  if (!places || places.length === 0) {
+    return (
+      <View style={styles.fallbackContainer}>
+        <Text style={styles.fallbackText}>
+          No places added yet - start adding some!
+        </Text>
+      </View>
+    );
+  }
 
   return (
     <FlatList
+      style={styles.list}
       data={places}
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => <PlacesItem place={item} />}
@@ -25,13 +26,16 @@ function PlacesList({ places }) {
 export default PlacesList;
 
 const styles = StyleSheet.create({
-    fallbackContainer: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    fallbackText: {
-        fontSize: 16,
-        color: Colors.primary200
-    }
+  list: {
+    margin: 24,
+  },
+  fallbackContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  fallbackText: {
+    fontSize: 16,
+    color: Colors.primary200,
+  },
 });
