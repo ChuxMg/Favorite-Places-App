59eca0d725c7cf7958f469b2b30d56d5651aae1e
ChuxMg <chuxmgbojikwe@gmail.com>
2024-07-14 16:52:39 +0700

    Environment Variables Updated

--- util/location.js
+++ util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyApuVGfGo_09ma_2rIu2TTgQqUiQRUmSS8";
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=size:mid%7Ccolor:red%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
